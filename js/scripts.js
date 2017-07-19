f94838f1b0a3ee9ee7fe6a56e788de3ceb8a6698
Jesse Bryan and Kim Schulze <jesse@>
2017-07-19 11:07:52 -0700

    Add spec 4: switch between player turns.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function Player(number, symbol) {
-  this.number = number;
+function Player(myTurn, symbol) {
+  this.myTurn = myTurn;
   this.symbol = symbol;
 }
 
@@ -18,54 +18,141 @@ function Board() {
 };
 
   var thisBoard;
-  var Player1;
-  var Player2;
+  var player1;
+  var player2;
 
 $(function() {
 
   $("#begin").click(function() {
     thisBoard = new Board();
-    Player1 = new Player(1, 'X');
-    Player2 = new Player(2, 'O');
+    player1 = new Player(true, 'X');
+    player2 = new Player(false, 'O');
   });
 
   $(".spaceOne").click(function(){
     if (thisBoard.spaces[1].value === ' ') {
-      $(".spaceOne").append("<img src='img/X.png'>")
-      thisBoard.spaces[1].value = 'X';
+      if (player1.myTurn) {
+        $(".spaceOne").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[1].value = player1.symbol;
+      } else {
+        $(".spaceOne").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[1].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
     }
   });
 
   $(".spaceTwo").click(function(){
-
+    if (thisBoard.spaces[2].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceTwo").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[2].value = player1.symbol;
+      } else {
+        $(".spaceTwo").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[2].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceThree").click(function(){
-
+    if (thisBoard.spaces[3].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceThree").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[3].value = player1.symbol;
+      } else {
+        $(".spaceThree").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[3].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceFour").click(function(){
-
+    if (thisBoard.spaces[4].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceFour").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[4].value = player1.symbol;
+      } else {
+        $(".spaceFour").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[4].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceFive").click(function(){
-
+    if (thisBoard.spaces[5].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceFive").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[5].value = player1.symbol;
+      } else {
+        $(".spaceFive").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[5].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceSix").click(function(){
-
+    if (thisBoard.spaces[6].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceSix").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[6].value = player1.symbol;
+      } else {
+        $(".spaceSix").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[6].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceSeven").click(function(){
-
+    if (thisBoard.spaces[7].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceSeven").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[7].value = player1.symbol;
+      } else {
+        $(".spaceSeven").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[7].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceEight").click(function(){
-
+    if (thisBoard.spaces[8].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceEight").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[8].value = player1.symbol;
+      } else {
+        $(".spaceEight").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[8].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
   $(".spaceNine").click(function(){
-
+    if (thisBoard.spaces[9].value === ' ') {
+      if (player1.myTurn) {
+        $(".spaceNine").append("<img src='img/" + player1.symbol + ".png'>");
+        thisBoard.spaces[9].value = player1.symbol;
+      } else {
+        $(".spaceNine").append("<img src='img/" + player2.symbol + ".png'>");
+        thisBoard.spaces[9].value = player1.symbol;
+      }
+      player1.myTurn = !player1.myTurn;
+      player2.myTurn = !player2.myTurn;
+    }
   });
 
 });
